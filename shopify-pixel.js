0da20e507c0caf29e16656a15b6ae056acb4cfea
Harm te Molder <mail@harmtemolder.com>
2025-03-15 07:17:24 +0100

    Add begin_checkout

--- shopify-pixel.js
+++ shopify-pixel.js
@@ -111,7 +111,61 @@ analytics.subscribe("checkout_completed", (event) => {
     shopify_event_type: event.type,
     user_email: checkout.email,
     user_id: checkout.customer.id,
-    user_logged_in: true,
+    user_logged_in: checkout.customer.id ? true : false,
+    user_phone: checkout.phone,
+  });
+});
+
+/**
+ * Push checkout_started as begin_checkout
+ * @see https://shopify.dev/docs/api/web-pixels-api/standard-events/checkout_started
+ * @see https://developers.google.com/analytics/devguides/collection/ga4/reference/events?client_type=gtm#begin_checkout
+ */
+analytics.subscribe("checkout_started", (event) => {
+  var checkout = event.data.checkout || {};
+  var coupons = (checkout.discountApplications || [])
+    .map(function (discountApplication) {
+      return discountApplication.title;
+    })
+    .join(",");
+  var items = (checkout.lineItems || []).map(shopifyCheckoutLineItemToGA4Item);
+  var paymentMethods = (checkout.transactions || [])
+    .map(function (transaction) {
+      return transaction.paymentMethod.name;
+    })
+    .join(",");
+  var deliveryOptions = (checkout.delivery.selectedDeliveryOptions || [])
+    .map(function (deliveryOption) {
+      return deliveryOption.title;
+    })
+    .join(",");
+
+  dataLayer.push({
+    checkout_token: checkout.token,
+    ecommerce: {
+      coupon: coupons,
+      currency: checkout.currencyCode,
+      items: items,
+      payment_type: paymentMethods,
+      shipping: checkout.shippingLine.price.amount,
+      shipping_tier: deliveryOptions,
+      subtotal: checkout.subtotalPrice.amount,
+      tax: checkout.totalTax.amount,
+      transaction_id: checkout.order.id,
+      value: checkout.totalPrice.amount,
+    },
+    event: "begin_checkout",
+    event_id: event.id,
+    event_timestamp: event.timestamp,
+    page_location: event.context.window.location.href,
+    page_title: event.context.document.title,
+    shopify_client_id: event.clientId,
+    shopify_event_name: event.name,
+    shopify_event_seq: event.seq,
+    shopify_event_type: event.type,
+    user_email: checkout.email,
+    user_id: checkout.customer.id,
+    user_logged_in: checkout.customer.id ? true : false,
     user_phone: checkout.phone,
   });
 });
